de971396d240f97130952a46fcfe9d101796ebec
tudoushell <zhoukaiit@gmail.com>
2026-05-13 17:22:09 +0800

    测试

--- ai-chat-service/src/main/java/com/elliot/ai/chat/config/ChatConfig.java
+++ ai-chat-service/src/main/java/com/elliot/ai/chat/config/ChatConfig.java
@@ -1,15 +1,43 @@
 package com.elliot.ai.chat.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@RequiredArgsConstructor
 @Configuration
 public class ChatConfig {
 
-    @Bean
-    public ChatClient chatClient(ChatClient.Builder builder) {
-        
-        return builder.build();
+    @Bean(name = "qwen3")
+    public ChatClient chatClient() {
+        OpenAiChatModel openChatModel = OpenAiChatModel.builder()
+                .openAiApi(OpenAiApi.builder()
+                        .baseUrl("http://localhost:11434")
+                        .apiKey("ollama")
+                        .build())
+                .defaultOptions(OpenAiChatOptions.builder()
+                        .model("qwen3-vl:2b")
+                        .build()).build();
+        return ChatClient.create(openChatModel);
     }
+
+    @Bean(name = "qwen4")
+    public ChatClient qwen4() {
+        OpenAiChatModel openChatModel = OpenAiChatModel.builder()
+                .openAiApi(OpenAiApi.builder()
+                        .baseUrl("http://localhost:11434")
+                        .apiKey("ollama")
+                        .build())
+                .defaultOptions(OpenAiChatOptions.builder()
+                        .model("qwen3-vl:2b")
+                        .build()).build();
+        return ChatClient.builder(openChatModel).defaultSystem("所有回答使用简洁的中文回答").build();
+    }
+
+
 }
--- ai-chat-service/src/main/java/com/elliot/ai/chat/controller/AiController.java
+++ ai-chat-service/src/main/java/com/elliot/ai/chat/controller/AiController.java
@@ -3,14 +3,19 @@ package com.elliot.ai.chat.controller;
 import com.elliot.ai.chat.dto.ChatRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 public class AiController {
 
     private final ChatClient chatClient;
+
+    public AiController(@Qualifier("qwen4") ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
 
     @GetMapping("/chat")
     public String chat(ChatRequest request) {
